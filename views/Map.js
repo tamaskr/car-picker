173d419f88b7208831c803d7c4e82ca49401442d
tamaskr <kralusztomi@gmail.com>
2022-02-15 17:11:26 +0200

    finished map

--- views/Map.js
+++ views/Map.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {Image, StyleSheet} from 'react-native';
 import {StatusBar} from 'expo-status-bar';
 import MapView from 'react-native-map-clustering';
 import {Marker} from 'react-native-maps';
+import {Icon} from 'react-native-elements';
 
 const Map = () => {
   const data = require('../data/locations.json');
   const cards = data.placemarks;
+  const marker = useRef();
 
+  // If there is a marker in focus, the callOut will be displayed
+  const prepareCallout = () => {
+    if (marker.current !== undefined && marker.current !== null)
+      marker.current.showCallout();
+  };
+
+  // Map out the basic data from locations.array and show on the map
   const mapMarkers = () => {
     return cards.map((card) => (
       <Marker
@@ -17,23 +26,56 @@ const Map = () => {
           longitude: card.coordinates[0],
         }}
         title={card.name}
+        onPress={() => {
+          setActiveMarkers(focusOnMarker(card));
+        }}
       >
+        <Icon reverse size={15} name="car" type="font-awesome" />
       </Marker>
     ));
   };
+
+  // If the user clicks on a car location, a ref will be set to the marker
+  // (to manually control showCallout)
+  const focusOnMarker = (card) => {
+    return (
+      <Marker
+        ref={marker}
+        key={card.vin}
+        coordinate={{
+          latitude: card.coordinates[1],
+          longitude: card.coordinates[0],
+        }}
+        title={card.name}
+        onPress={() => {
+          marker.current = undefined;
+          setActiveMarkers(mapMarkers());
+        }}
+      ></Marker>
+    );
+  };
+  const [activeMarkers, setActiveMarkers] = useState(mapMarkers());
   return (
     <>
       <MapView
+        provider="google"
+        loadingEnabled={true}
         style={{...StyleSheet.absoluteFillObject}}
         showsUserLocation={true}
+        showsMyLocationButton={true}
+        rotateEnabled={false}
+        zoomTapEnabled={false}
+        onMarkerPress={() => {
+          prepareCallout();
+        }}
         initialRegion={{
-          latitude: 37.1,
-          longitude: -95.7,
-          latitudeDelta: 10,
-          longitudeDelta: 45,
+          latitude: 53,
+          longitude: 10,
+          latitudeDelta: 35,
+          longitudeDelta: 25,
         }}
       >
-        {mapMarkers()}
+        {activeMarkers}
       </MapView>
       <StatusBar style="auto" />
     </>
